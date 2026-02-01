0b21b9d73b32c923151ebd6fc887ea97d748bdd1
junyoungKim <wnsdud1900427@gmail.com>
2026-02-02 00:46:48 +0900

    feat: 비밀번호 길이 검증 조건 추가 - 16자 초과 비밀번호 불가 처리

--- apps/commerce-api/src/test/java/com/loopers/user/PasswordValidator.java
+++ apps/commerce-api/src/test/java/com/loopers/user/PasswordValidator.java
@@ -2,7 +2,11 @@ package com.loopers.user;
 
 public class PasswordValidator {
     public static boolean validate(String password) {
-        if(password == null || password.length() < 8) {
+        if (password == null) {
+            return false;
+        } else if (password.length() < 8) {
+            return false;
+        } else if (password.length() > 16) {
             return false;
         }
         return true;
--- apps/commerce-api/src/test/java/com/loopers/user/PasswordValidatorTest.java
+++ apps/commerce-api/src/test/java/com/loopers/user/PasswordValidatorTest.java
@@ -7,9 +7,21 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class PasswordValidatorTest {
 
     @Test
-    public void 비밀번호_8자_미만이면_실패 () {
+    public void 비밀번호_8자_미만이면_실패() {
         //given
         String password = "1234";
+
+        //when
+        boolean result = PasswordValidator.validate(password);
+
+        //then
+        assertThat(result).isFalse();
+    }
+
+    @Test
+    public void 비밀번호_16자_초과하면_실패() {
+        //given
+        String password = "12345678912345678";
 
         //when
         boolean result = PasswordValidator.validate(password);
