5b94f7bdf2982b2015e5f57c55dc16d315fdd47a
junyoungKim <wnsdud1900427@gmail.com>
2026-02-02 21:21:05 +0900

    test: 회원가입 입력값 유효성 테스트 껍데기 추가

---
+++ apps/commerce-api/src/test/java/com/loopers/user/SignUpRequestValidationTest.java
@@ -0,0 +1,42 @@
+package com.loopers.user;
+
+import org.junit.jupiter.api.Test;
+
+public class SignUpRequestValidationTest {
+
+    @Test
+    public void 필수값_누락시_실패() {
+        //given
+
+        //when
+
+        //then
+    }
+
+    @Test
+    public void 이름이_비어있으면_실패() {
+        //given
+
+        //when
+
+        //then
+    }
+
+    @Test
+    public void 이메일_형식_불일치시_실패() {
+        //given
+
+        //when
+
+        //then
+    }
+
+    @Test
+    public void 생년월일_형식_불일치시_실패() {
+        //given
+
+        //when
+
+        //then
+    }
+}
