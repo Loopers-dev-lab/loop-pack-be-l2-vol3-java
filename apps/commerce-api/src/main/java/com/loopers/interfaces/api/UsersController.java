0768937f810e1194ad7d3555eb9f56790a91865c
김용권 <yongkwon.kim@ikoob.com>
2026-02-02 16:52:17 +0900

    feat : 회원가입 api 응답객체 타입 변경

--- apps/commerce-api/src/main/java/com/loopers/interfaces/api/UsersController.java
+++ apps/commerce-api/src/main/java/com/loopers/interfaces/api/UsersController.java
@@ -1,6 +1,7 @@
 package com.loopers.interfaces.api;
 
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class UsersController {
 
     @PostMapping
-    public String signUp() {
-        return "ok";
+    public ApiResponse<String> signUp(@RequestBody UserSignUpRequestDto requestDto) {
+        return ApiResponse.success("ok");
     }
 }
