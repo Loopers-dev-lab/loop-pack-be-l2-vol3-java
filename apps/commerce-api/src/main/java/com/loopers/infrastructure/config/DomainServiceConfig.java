dbccdb5fb41c3766c65fead25f0686ab9d76eb8d
yoon-yoo-tak <dbsdbxkrzz@naver.com>
2026-02-03 21:27:01 +0900

    refactor : 도메인 계층의 service클래스에 Spring 의존성 제거

---
+++ apps/commerce-api/src/main/java/com/loopers/infrastructure/config/DomainServiceConfig.java
@@ -0,0 +1,16 @@
+package com.loopers.infrastructure.config;
+
+import com.loopers.domain.user.PasswordEncryptor;
+import com.loopers.domain.user.UserRepository;
+import com.loopers.domain.user.UserService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DomainServiceConfig {
+
+    @Bean
+    public UserService userService(UserRepository userRepository, PasswordEncryptor passwordEncryptor) {
+        return new UserService(userRepository, passwordEncryptor);
+    }
+}
