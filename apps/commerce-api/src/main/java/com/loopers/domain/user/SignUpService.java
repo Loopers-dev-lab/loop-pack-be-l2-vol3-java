7e7e913b6d3fab7c257a97509421a9edb8deee61
hey-sion <heysion007005@gmail.com>
2026-02-04 17:20:59 +0900

    feat: 회원가입 서비스 클래스 레벨에 @Transactional 추가

--- apps/commerce-api/src/main/java/com/loopers/domain/user/SignUpService.java
+++ apps/commerce-api/src/main/java/com/loopers/domain/user/SignUpService.java
@@ -1,10 +1,12 @@
 package com.loopers.domain.user;
 
 import com.loopers.application.user.SignUpCommand;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@Transactional
 @RequiredArgsConstructor
 public class SignUpService {
     private final SignUpValidator signUpValidator;
