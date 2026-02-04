b63e5469a807bf7d2661527ddd9fab515c9a0d5d
hey-sion <heysion007005@gmail.com>
2026-02-04 15:03:08 +0900

    refactor: SignUpCommand에 from 팩토리 메서드 추가

--- apps/commerce-api/src/main/java/com/loopers/application/user/SignUpCommand.java
+++ apps/commerce-api/src/main/java/com/loopers/application/user/SignUpCommand.java
@@ -1,11 +1,23 @@
 package com.loopers.application.user;
 
+import com.loopers.interfaces.api.user.dto.CreateUserRequestV1;
+
 import java.time.LocalDate;
 
 public record SignUpCommand(
-    String loginId,
-    String password,
-    String name,
-    LocalDate birthDate,
-    String email
-) {}
+        String loginId,
+        String password,
+        String name,
+        LocalDate birthDate,
+        String email
+) {
+    public static SignUpCommand from(CreateUserRequestV1 request) {
+        return new SignUpCommand(
+                request.getLoginId(),
+                request.getPassword(),
+                request.getName(),
+                request.getBirthDate(),
+                request.getEmail()
+        );
+    }
+}
