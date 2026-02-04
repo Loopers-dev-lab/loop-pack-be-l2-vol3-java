5eec4e97e39d7f793cdf3987bcdb051d044f03d1
hey-sion <heysion007005@gmail.com>
2026-02-04 11:15:29 +0900

    refactor: 요청 dto 빈값체크 어노테이션 변경 @NotNull -> @NotBlank

--- apps/commerce-api/src/main/java/com/loopers/interfaces/api/user/dto/CreateUserRequestV1.java
+++ apps/commerce-api/src/main/java/com/loopers/interfaces/api/user/dto/CreateUserRequestV1.java
@@ -1,6 +1,7 @@
 package com.loopers.interfaces.api.user.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Getter;
@@ -10,19 +11,19 @@ import java.time.LocalDate;
 @Getter
 @Builder
 public class CreateUserRequestV1 {
-    @NotNull(message = "로그인 ID는 필수값입니다.")
+    @NotBlank(message = "로그인 ID는 필수값입니다.")
     private String loginId;
 
-    @NotNull(message = "비밀번호는 필수값입니다.")
+    @NotBlank(message = "비밀번호는 필수값입니다.")
     private String password;
 
-    @NotNull(message = "이름은 필수값입니다.")
+    @NotBlank(message = "이름은 필수값입니다.")
     private String name;
 
-    @NotNull(message = "생년월일은 필수값입니다.")
+    @NotBlank(message = "생년월일은 필수값입니다.")
     @JsonFormat(pattern = "yyyy-MM-dd")
     private LocalDate birthDate;
 
-    @NotNull(message = "이메일은 필수값입니다.")
+    @NotBlank(message = "이메일은 필수값입니다.")
     private String email;
 }
