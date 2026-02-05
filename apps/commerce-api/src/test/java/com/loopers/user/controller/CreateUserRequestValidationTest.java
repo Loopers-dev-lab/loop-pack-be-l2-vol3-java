6c4660c95ddeedfe331f0d1750cfb7ef898a9d0f
junyoungKim <wnsdud1900427@gmail.com>
2026-02-05 23:50:27 +0900

    test: 로그인 id 검증 테스트 케이스 추가

--- apps/commerce-api/src/test/java/com/loopers/user/controller/CreateUserRequestValidationTest.java
+++ apps/commerce-api/src/test/java/com/loopers/user/controller/CreateUserRequestValidationTest.java
@@ -5,6 +5,7 @@ import jakarta.validation.ConstraintViolation;
 import jakarta.validation.Validation;
 import jakarta.validation.Validator;
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -88,5 +89,28 @@ public class CreateUserRequestValidationTest {
         assertThat(violations).hasSize(1);
         ConstraintViolation<CreateUserRequest> violation = violations.iterator().next();
         assertThat(violation.getPropertyPath().toString()).isEqualTo("birthDate");
+    }
+
+    @Test
+    void 로그인ID에_영문_숫자_외_문자_포함_시_실패() {
+        //given
+        String id = "test@123";
+        String password = "pw";
+        String name = "name";
+        String birthDate = "1990-01-01";
+        String email = "test@test.com";
+        CreateUserRequest request = new CreateUserRequest(id, password, name, birthDate, email);
+
+        //when
+        Set<ConstraintViolation<CreateUserRequest>> violations = validator.validate(request);
+
+        //then
+        assertThat(violations).hasSize(1);
+        ConstraintViolation<CreateUserRequest> violation = violations.iterator().next();
+        assertThat(violation.getPropertyPath().toString()).isEqualTo("loginId");
+        assertThat(violation.getConstraintDescriptor()
+                .getAnnotation()
+                .annotationType())
+                .isEqualTo(Pattern.class);
     }
 }
