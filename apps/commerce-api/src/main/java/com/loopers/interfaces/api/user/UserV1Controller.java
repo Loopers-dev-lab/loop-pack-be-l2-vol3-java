75032b46be2f0121bc7c4a69996e721cdfe28a75
임나현 <iohyeonsz@gmail.com>
2026-02-07 20:56:44 +0900

    fix: 불필요한 UserFacade 계층 제거 - 패스스루만 수행하는 UserFacade 삭제 - Controller에서 UserInfo.from() 직접 호출로 변경

--- apps/commerce-api/src/main/java/com/loopers/interfaces/api/user/UserV1Controller.java
+++ apps/commerce-api/src/main/java/com/loopers/interfaces/api/user/UserV1Controller.java
@@ -1,6 +1,5 @@
 package com.loopers.interfaces.api.user;
 
-import com.loopers.application.user.UserFacade;
 import com.loopers.application.user.UserInfo;
 import com.loopers.domain.user.User;
 import com.loopers.interfaces.api.ApiResponse;
@@ -14,16 +13,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/users")
 public class UserV1Controller implements UserV1ApiSpec {
 
-    private final UserFacade userFacade;
-
-    public UserV1Controller(UserFacade userFacade) {
-        this.userFacade = userFacade;
-    }
-
     @GetMapping("/me")
     @Override
     public ApiResponse<UserV1Response.UserResponse> getUser(@AuthUser User user) {
-        UserInfo info = this.userFacade.getUser(user);
-        return ApiResponse.success(UserV1Response.UserResponse.from(info));
+        return ApiResponse.success(UserV1Response.UserResponse.from(UserInfo.from(user)));
     }
 }
