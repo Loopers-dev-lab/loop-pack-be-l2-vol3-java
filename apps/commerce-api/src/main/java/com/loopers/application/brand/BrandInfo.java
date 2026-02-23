7f359559c3ddb598d12c1232cb945558b92700d6
hey-sion <heysion007005@gmail.com>
2026-02-23 20:47:11 +0900

    feat: 누락 파일 추가

---
+++ apps/commerce-api/src/main/java/com/loopers/application/brand/BrandInfo.java
@@ -0,0 +1,25 @@
+package com.loopers.application.brand;
+
+import com.loopers.domain.brand.Brand;
+
+import java.time.ZonedDateTime;
+
+public record BrandInfo(
+        Long id,
+        String name,
+        String description,
+        ZonedDateTime createdAt,
+        ZonedDateTime updatedAt,
+        ZonedDateTime deletedAt
+) {
+    public static BrandInfo from(Brand brand) {
+        return new BrandInfo(
+                brand.getId(),
+                brand.getName(),
+                brand.getDescription(),
+                brand.getCreatedAt(),
+                brand.getUpdatedAt(),
+                brand.getDeletedAt()
+        );
+    }
+}
---
+++ apps/commerce-api/src/main/java/com/loopers/application/product/ProductInfo.java
@@ -0,0 +1,33 @@
+package com.loopers.application.product;
+
+import com.loopers.domain.product.Product;
+
+import java.time.ZonedDateTime;
+
+public record ProductInfo(
+        Long id,
+        Long brandId,
+        String name,
+        String description,
+        Integer price,
+        Integer stockQuantity,
+        Product.Visibility visibility,
+        ZonedDateTime createdAt,
+        ZonedDateTime updatedAt,
+        ZonedDateTime deletedAt
+) {
+    public static ProductInfo from(Product product) {
+        return new ProductInfo(
+                product.getId(),
+                product.getBrandId(),
+                product.getName(),
+                product.getDescription(),
+                product.getPrice(),
+                product.getStockQuantity(),
+                product.getVisibility(),
+                product.getCreatedAt(),
+                product.getUpdatedAt(),
+                product.getDeletedAt()
+        );
+    }
+}
