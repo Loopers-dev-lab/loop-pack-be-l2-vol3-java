8dd9d1181a1a0cc003c08598f69f181d32a48959
hey-sion <heysion007005@gmail.com>
2026-03-06 08:53:15 +0900

    refactor: dto 레이어 위반 수정

--- apps/commerce-api/src/main/java/com/loopers/application/user/SignUpCommand.java
+++ apps/commerce-api/src/main/java/com/loopers/application/user/SignUpCommand.java
@@ -1,7 +1,5 @@
 package com.loopers.application.user;
 
-import com.loopers.interfaces.api.user.dto.UserV1Dto;
-
 import java.time.LocalDate;
 
 public record SignUpCommand(
@@ -11,13 +9,4 @@ public record SignUpCommand(
         LocalDate birthDate,
         String email
 ) {
-    public static SignUpCommand from(UserV1Dto.CreateRequest request) {
-        return new SignUpCommand(
-                request.loginId(),
-                request.password(),
-                request.name(),
-                request.birthDate(),
-                request.email()
-        );
-    }
 }
