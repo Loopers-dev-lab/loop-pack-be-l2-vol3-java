03da6642356977d98c25e52ec8135f5ba7b94245
YoHanKi <alcuz137@gmail.com>
2026-03-18 22:01:35 +0900

    bug : pg 응답 파싱 오류 수정.

--- apps/commerce-api/src/main/java/com/loopers/infrastructure/payment/config/PgFeignConfig.java
+++ apps/commerce-api/src/main/java/com/loopers/infrastructure/payment/config/PgFeignConfig.java
@@ -1,9 +1,15 @@
 package com.loopers.infrastructure.payment.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.loopers.infrastructure.payment.pg.PgErrorDecoder;
+import feign.Request;
 import feign.Retryer;
+import feign.codec.Decoder;
 import feign.codec.ErrorDecoder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+
+import java.util.concurrent.TimeUnit;
 
 public class PgFeignConfig {
 
@@ -15,5 +21,15 @@ public class PgFeignConfig {
     @Bean
     public ErrorDecoder errorDecoder() {
         return new PgErrorDecoder();
+    }
+
+    @Bean
+    public Decoder decoder(ObjectMapper objectMapper) {
+        return new PgResponseDecoder(objectMapper);
+    }
+
+    @Bean
+    public Request.Options requestOptions(@Value("${pg.feign.read-timeout:2000}") long readTimeoutMs) {
+        return new Request.Options(1000, TimeUnit.MILLISECONDS, readTimeoutMs, TimeUnit.MILLISECONDS, true);
     }
 }
