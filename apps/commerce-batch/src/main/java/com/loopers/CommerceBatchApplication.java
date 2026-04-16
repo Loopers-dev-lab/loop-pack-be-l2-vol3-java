518e073c9ec0f9155794f7066eaecf05a74cd8d8
shAn-kor <kjn0406@naver.com>
2026-04-17 01:13:18 +0900

    chore(batch): provide application clock bean

--- apps/commerce-batch/src/main/java/com/loopers/CommerceBatchApplication.java
+++ apps/commerce-batch/src/main/java/com/loopers/CommerceBatchApplication.java
@@ -4,17 +4,26 @@ import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.annotation.Bean;
 
+import java.time.Clock;
+import java.time.ZoneId;
 import java.util.TimeZone;
 
 @ConfigurationPropertiesScan
 @SpringBootApplication
 public class CommerceBatchApplication {
+    private static final String DEFAULT_ZONE_ID = "Asia/Seoul";
 
     @PostConstruct
     public void started() {
         // set timezone
-        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+        TimeZone.setDefault(TimeZone.getTimeZone(DEFAULT_ZONE_ID));
+    }
+
+    @Bean
+    public Clock clock() {
+        return Clock.system(ZoneId.of(DEFAULT_ZONE_ID));
     }
 
     public static void main(String[] args) {
